4bc0c2178baab37cb434e4fe81e66784c7531a6a
Kristian <kristians93@hotmail.com>
2024-10-09 15:26:49 +0200

    add api versioning

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,12 @@ public class Program
         
         // Add services to the container.
         builder.Services.AddAuthorization();
-
         builder.Services.AddCoreServices();
         builder.Services.AddInfrastructure();
+        
+        
+        // API Configuration
+        builder.Services.AddApiConfiguration();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
---
+++ API/RegisterApiConfiguration.cs
@@ -0,0 +1,26 @@
+using Asp.Versioning;
+
+namespace API;
+
+public static class RegisterApiConfiguration
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+    {
+        // Versioning
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1);
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new UrlSegmentApiVersionReader(),
+                new HeaderApiVersionReader("X-Api-Version"));
+        }).AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'V";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
+        return services;
+    }
+}
