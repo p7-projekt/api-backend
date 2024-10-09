ef74edcb1002425d969430775cba4451487b7e4d
Kristian <kristians93@hotmail.com>
2024-10-09 16:20:50 +0200

    Missing async in method

--- Core/StudentService.cs
+++ Core/StudentService.cs
@@ -13,7 +13,7 @@ public class StudentService
     }
 
 
-    public Task HentNogleStudents()
+    public async Task HentNogleStudents()
     {
         var url = "http://127.0.0.1:8080/submit";
         using var client = new HttpClient();
