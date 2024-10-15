406033f8a5e32edf2201195067a60f7eeafc3829
Jonathan Myrtue Jensen <jojens15@student.aau.dk>
2024-10-15 12:08:29 +0200

    begins on creating exercise

---
+++ API/ExerciseEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using Asp.Versioning.Builder;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace API
+{
+    public static class ExerciseEndpoints
+    {
+        public static WebApplication UseExerciseEndpouints(this WebApplication app)
+        {
+            ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+                .HasApiVersion(new ApiVersion(1))
+                .ReportApiVersions()
+                .Build();
+
+            var exerciseV1 = app.MapGroup("v{version.apiVersion}/students").WithApiVersionSet(apiVersionSet).WithTags("Exercise");
+
+            exerciseV1.MapPost("/create", async Task<Results<Ok<string>, NotFound>> (ExerciseService)) =>
+
+            return app;
+        }
+    }
+}
