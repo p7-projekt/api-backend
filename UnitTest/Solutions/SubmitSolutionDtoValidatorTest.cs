215fdbb2d8c6f8464fc38a3fa38d5776fa510590
Kristian <kristians93@hotmail.com>
2024-11-03 10:45:23 +0100

    solution validator

---
+++ UnitTest/Solutions/SubmitSolutionDtoValidatorTest.cs
@@ -0,0 +1,40 @@
+using Core.Solutions.Models;
+using Core.Solutions.Validators;
+
+namespace UnitTest.Solutions;
+
+public class SubmitSolutionDtoValidatorTest
+{
+	[Fact]
+	public void SubmitSolutionDtoValidatorTest_ShouldReturn_Ok()
+	{
+		var validator = new SubmitSolutionDtoValidator();
+		var dto = new SubmitSolutionDto(1, "test");
+		
+		var result = validator.Validate(dto);
+		
+		Assert.True(result.IsValid);
+	}
+	
+	[Fact]
+	public void SubmitSolutionDtoValidatorTest_ShouldReturn_FailEmptySolution()
+	{
+		var validator = new SubmitSolutionDtoValidator();
+		var dto = new SubmitSolutionDto(1, "");
+		
+		var result = validator.Validate(dto);
+		
+		Assert.False(result.IsValid);
+	}
+	
+	[Fact]
+	public void SubmitSolutionDtoValidatorTest_ShouldReturn_FailId()
+	{
+		var validator = new SubmitSolutionDtoValidator();
+		var dto = new SubmitSolutionDto(0, "test");
+		
+		var result = validator.Validate(dto);
+		
+		Assert.False(result.IsValid);
+	}
+}
