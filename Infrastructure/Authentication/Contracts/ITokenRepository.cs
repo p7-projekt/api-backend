6a834a7193280fa3403807b7827521aa601c65d3
JMyrtue <jojens15@student.aau.dk>
2024-11-05 18:09:01 +0100

    adds test cases to TokenServiceTest and UserServiceTest; Make minor modifications on tested methods to simplify testing

--- Infrastructure/Authentication/Contracts/ITokenRepository.cs
+++ Infrastructure/Authentication/Contracts/ITokenRepository.cs
@@ -1,3 +1,4 @@
+using FluentResults;
 using Infrastructure.Authentication.Models;
 
 namespace Infrastructure.Authentication.Contracts;
@@ -6,6 +7,6 @@ public interface ITokenRepository
 {
 	Task InsertTokenAsync(RefreshToken token);
 
-	Task<RefreshToken?> GetAccessTokenByRefreshTokenAsync(string refreshToken);
+	Task<Result<RefreshToken>> GetAccessTokenByRefreshTokenAsync(string refreshToken);
 	Task DeleteRefreshTokenByUserIdAsync(int userId);
 }
