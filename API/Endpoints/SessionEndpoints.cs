8c82b4ddb00a34007425d46d3b7b21b0e0f4293a
Jonas Poulsen <62690634+Schoogle@users.noreply.github.com>
2024-11-22 16:09:21 +0100

    Update SessionEndpoints.cs

--- API/Endpoints/SessionEndpoints.cs
+++ API/Endpoints/SessionEndpoints.cs
@@ -121,7 +121,7 @@ public static class SessionEndpoints
         }).WithRequestValidation<JoinSessionDto>();
 
         //Get exercises in timed_session
-        sessionV1Group.MapGet("/{sessionId:int}/timed_session", async Task<Results<Ok<GetExercisesInSessionCombinedInfo>, NotFound, BadRequest>> (int sessionId, ClaimsPrincipal principal,
+        sessionV1Group.MapGet("/{sessionId:int}/timedSession", async Task<Results<Ok<GetExercisesInSessionCombinedInfo>, NotFound, BadRequest>> (int sessionId, ClaimsPrincipal principal,
                 ISessionService sessionService) =>
         {
             var userId = principal.Claims.First(c => c.Type == ClaimTypes.UserData).Value;
