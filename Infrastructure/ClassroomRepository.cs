e505987ac42a4304bedb69874dd698c39ea71f58
Jonathan Myrtue Jensen <jojens15@student.aau.dk>
2024-11-27 12:50:16 +0100

    removes unused imports from classroom repo

--- Infrastructure/ClassroomRepository.cs
+++ Infrastructure/ClassroomRepository.cs
@@ -2,22 +2,10 @@
 using Core.Classrooms.Models;
 using Core.Languages.Models;
 using Core.Sessions.Contracts;
-using Core.Sessions.Models;
 using Dapper;
 using FluentResults;
 using Infrastructure.Persistence.Contracts;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Logging;
-using Quartz.Simpl;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure;
 
